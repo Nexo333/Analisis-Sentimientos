9ae7ee0fa5fbbebd6f44df61452283aa7b6d6694
Nelson Romo <nelson.romo@yachaytech.edu.ec>
2026-09-17 10:36:14 -0500

    Updated sentiment analysis app

--- GUI_Sentimientos_API.py
+++ GUI_Sentimientos_API.py
@@ -1,49 +1,74 @@
+# Importamos las librerías necesarias
+
 import streamlit as st
 from transformers import pipeline
-import torch
 
+# Cargar el modelo una sola vez y guardarlo en caché, evitando que carguemos el modelo cada ve que se ejecute la transformacion
 
-# Modelo
-modelo = "tabularisai/multilingual-sentiment-analysis"
+@st.cache_resource
 
+# Usaremos el modelo: clapAI/mmBERT-small-multilingual-sentiment para analisis de sentimientos
 
-# Cargar modelo
-sentimientos = pipeline(
-    "sentiment-analysis",
-    model=modelo,
-    device=0 if torch.cuda.is_available() else -1
-)
+def cargar_modelo():
 
+    modelo = "clapAI/mmBERT-small-multilingual-sentiment"
 
-# Título
-st.title("Análisis de Sentimientos")
+    return pipeline(
+        "text-classification",
+        model=modelo,
+        device=-1
+    )
 
+sentimientos = cargar_modelo()
+
+# Interfaz gráfica
+
+# Titulos y subtitulos
+
+st.title("ANÁLISIS DE SENTIMIENTOS")
 st.write("Ingrese una frase para analizar el sentimiento:")
+st.caption("Ejemplo: Estoy muy feliz con mi compra")
 
+# Cuadro para ingresar el texto
 
-# Casilla de texto
-texto = st.text_area(
-    "Texto:",
-    placeholder="Ejemplo: Estoy muy feliz con el producto que compré."
-)
+texto = st.text_area("Texto", key="texto", height=120, label_visibility="collapsed")
 
+# Boton limpiar
 
-# Botón
-if st.button("Analizar sentimiento"):
+def limpiar(): st.session_state["texto"] = ""
 
-    if texto:
+# Posiciones de los botones
 
-        # Analizar sentimiento
+col1, col2 = st.columns([1, 3])
+
+with col1: analizar = st.button("Analizar sentimiento")
+with col2: st.button("Limpiar", on_click=limpiar)
+
+# Analizar cuando se presiona el botón analizar
+
+if analizar:
+
+    if texto.strip():
+
         resultado = sentimientos(texto)[0]
 
-        # Obtener resultado
         sentimiento = resultado["label"]
         confianza = resultado["score"] * 100
 
-        # Mostrar resultado
+        clases = {
+            "positive": "😁 Positivo",
+            "neutral": "😐 Neutral",
+            "negative": "😞 Negativo"
+        }
+
+        mostrar_sentimiento = clases.get(
+            sentimiento,
+            sentimiento
+        )
+
         st.subheader("Resultado")
 
-        st.write("Sentimiento:", sentimiento)
+        st.write("Sentimiento:", mostrar_sentimiento)
         st.write("Confianza:", round(confianza, 2), "%")
 
     else:
